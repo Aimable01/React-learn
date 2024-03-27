b99023844ff42edfb89ac2e9d1df6a5fe0f6fc83
unknown <aimable.kwizera14@gmail.com>
2024-03-27 07:07:14 +0200

    Add props

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Food from "./Food";
 import Card from "./Card";
+import Student from "./Student";
 
 function App() {
   return (
@@ -11,6 +12,10 @@ function App() {
       <Card />
       <Card />
       <Card />
+      <Student name="Spongebob" age={32} isStudent={true} />
+      <Student name="Patrick" age={23} isStudent={false} />
+      <Student name="Sandy" age={27} isStudent={true} />
+      <Student />
       <Food />
       <Footer />
     </>

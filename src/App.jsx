aba449fed27faffc7621c7420560900b2dd26460
unknown <aimable.kwizera14@gmail.com>
2024-04-02 21:05:50 +0200

    the main entry

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import Footer from "./Footer";
 import Food from "./Food";
 import Card from "./Card";
 import Student from "./Student";
+import Greeting from "./Greeting";
+import Button from "./Button";
+import MyHook from "./MyHook";
+import Todo from "./Todo";
+import Slider from "./Slider";
+import Search from "./Search";
+import Fetch from "./Fetch";
+import WeatherApp from "./WeatherApp";
 
 function App() {
   return (
@@ -16,7 +24,15 @@ function App() {
       <Student name="Patrick" age={23} isStudent={false} />
       <Student name="Sandy" age={27} isStudent={true} />
       <Student />
+      <Greeting isLoggedIn={true} username="BroCode" />
       <Food />
+      <Button />
+      <MyHook />
+      <Todo />
+      <Slider />
+      <Search />
+      <Fetch />
+      <WeatherApp />
       <Footer />
     </>
   );
